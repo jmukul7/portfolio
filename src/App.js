45abed34cf2e3aaa756bbf163fb33fa33e21fa5e
Mukul Joshi <68437448+jmukul7@users.noreply.github.com>
2022-09-21 23:28:58 +0530

    reback

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1>Mukul Joshi</h1>
-     <form action="http://localhost:3330/login?" method="post">
-        <input type="text" name="name" id="'1" /><br/>
-        <input type="password" name="password" id="2" /><br/>
-        <input type="submit"/>
-    </form>
+    
     </div>
   );
 }
